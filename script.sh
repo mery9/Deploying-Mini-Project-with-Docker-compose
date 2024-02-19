e0a07ff91b436090600d11f10dea0b2b6a4aebaf
Kantinan Boonyingsathit <kaseam008@gmail.com>
2024-02-19 19:32:06 +0700

    added the sudo apt install docker-compose command

--- script.sh
+++ script.sh
@@ -15,6 +15,8 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+sudo apt install docker-compose
+
 sudo apt update
 sudo apt install git
 git --version
