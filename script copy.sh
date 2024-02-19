62e8cb05540d75b2dfe62b1ae973472508ac449c
Kantinan Boonyingsathit <kaseam008@gmail.com>
2024-02-19 20:27:18 +0700

    added kubeadm, kubelet, and kubectl

--- script copy.sh
+++ script copy.sh
@@ -39,6 +39,17 @@ sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plug
 # Install Docker Compose
 sudo apt -y install docker-compose
 
+# Install kubeadm, kubelet, and kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
 # Build and run the project with Docker Compose
 sudo docker-compose build
 sudo docker-compose up -d
